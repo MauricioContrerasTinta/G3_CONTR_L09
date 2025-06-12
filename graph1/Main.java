82b1c80da9cd6be491e8a2fc1fbf002f5462f102
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-06-12 14:07:16 -0500

    insertar aristas

--- graph1/Main.java
+++ graph1/Main.java
@@ -3,11 +3,16 @@ package graph1;
 public class Main {
     public static void main(String[] args) {
         GraphLink<String> graph = new GraphLink<>();
-
         // Insertar vértices
         graph.insertVertex("A");
         graph.insertVertex("B");
         graph.insertVertex("C");
         graph.insertVertex("D");
+
+        // Insertar aristas
+        graph.insertEdge("A", "B", 2);
+        graph.insertEdge("A", "C", 3);
+        graph.insertEdge("B", "D", 4);
+        graph.insertEdge("C", "D");
     }
 }

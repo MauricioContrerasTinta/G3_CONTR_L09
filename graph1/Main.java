c75cfcca53e4d37d8a8242783dcda04986e6e326
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-06-12 14:06:46 -0500

    insertar vertcies

--- graph1/Main.java
+++ graph1/Main.java
@@ -1,5 +1,13 @@
 package graph1;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        GraphLink<String> graph = new GraphLink<>();
+
+        // Insertar vértices
+        graph.insertVertex("A");
+        graph.insertVertex("B");
+        graph.insertVertex("C");
+        graph.insertVertex("D");
+    }
 }

1af3a2e0b76b6d9a08f32fbb224721a67e7fe7cf
MauricioContrerasTinta <165443093+MauricioContrerasTinta@users.noreply.github.com>
2025-06-24 16:10:20 -0500

    Update GraphLink.java

--- graph1/GraphLink.java
+++ graph1/GraphLink.java
@@ -2,16 +2,7 @@ package graph1;
 import listlinked.ListLinked;
 import listlinked.Node;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class GraphLink<E> {
     private ListLinked<Vertex<E>> listVertex;
